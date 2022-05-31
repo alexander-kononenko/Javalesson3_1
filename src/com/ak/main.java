952346e59287cf1c4790c2350f2417f3d30b45b9
kononenko192@gmail.com <alexander-kononenko>
2022-05-31 20:17:11 +0300

    new

--- src/com/ak/main.java
+++ src/com/ak/main.java
@@ -1,4 +1,17 @@
 package com.ak;
 
 public class main {
+    public static void main(String[] args) {
+        DanceRob rob = new BalletDancingRobot();
+        printer(rob);
+        printer(new FolkDancingRobot());
+        printer(new JazzDancingRobot());
+    }
+
+    private static void printer(DanceRob rob) {
+        System.out.println("________________");
+        System.out.println(rob.getDanceType1());
+        System.out.println(rob.getDanceType2());
+        System.out.println("________________");
+    }
 }
